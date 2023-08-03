a08794d505ce6bc86979816851b6319f531d0bc8
fredzo <fborry@free.fr>
2023-08-03 21:07:06 +0200

    Fixed some protocol desciptions. Added ELT(DT) and RTS Location parsing.

--- SarsatJRX-code/src/Beacon.cpp
+++ SarsatJRX-code/src/Beacon.cpp
@@ -1,5 +1,6 @@
 #include "Beacon.h"
 
+
 Beacon::Beacon(byte frameBuffer[])
 {   // Get a local copy of the original frame
     memcpy(frame,frameBuffer,SIZE);
@@ -42,7 +43,6 @@ void Beacon::parseFrame()
     {
         if(protocolFlag == 1) 
         {   // User loc protocol trame longue
-            // TODO : Check if protocol code = 1,2,3,6,7
             location.latitude.orientation = (frame[13] & 0x10) >> 4;   
             location.latitude.degrees = ((frame[13] & 0x0F) << 3 | (frame[14] & 0xE0) >> 5);
             location.latitude.minutes = (frame[14] & 0x1E) >> 1;
@@ -73,20 +73,21 @@ void Beacon::parseFrame()
                     desciption = F("Second Generation Beacon");
                     break;
                 case 0b110 :
-                    desciption += F("EPIRB-Radio Call Sign User P.");
+                    desciption = F("EPIRB-Radio Call Sign User P.");
                     break;
                 case 0b111 :
-                    desciption += F("Test User Location Protocol");
+                    desciption = F("Test User Location Protocol");
                     break;
                 default:
-                    desciption += F("Unknown");
+                    desciption = F("Unknown");
             }
+            // Identification data in bits 40-83
         }
         else // if (protocolFlag == 0)
-        {
-            if(protocolCode == 8 /*ELT*/ || protocolCode == 10 /* EPIRB */ 
-                || protocolCode == 11 /* PLB */ || protocolCode == 16 /* National Test Location Protocol */)
-            { //Nat loc protocol / trame longue
+        {   // Location decoding according to protocol code
+            if(protocolCode == 0b1000 /*ELT*/ || protocolCode == 0b1010 /* EPIRB */ 
+                || protocolCode == 0b1011 /* PLB */ || protocolCode == 0b1111 /* National Test Location Protocol */)
+            {   // Nat loc protocol
                 latoffset = (frame[14] & 0x80) >> 7;   // Latitude NAT LOCATION
                 location.latitude.orientation = (frame[7] & 0x20) >> 5;   
                 location.latitude.degrees = ((frame[7] & 0x1F) << 2 | (frame[8] & 0xC0) >> 6);
@@ -116,7 +117,7 @@ void Beacon::parseFrame()
                     }
                 }
 
-                lonoffset = (frame[14] & 0x01);       //Longitude NAT LOCATION
+                lonoffset = (frame[14] & 0x01);
                 location.longitude.orientation = (frame[8] & 0x01);           
                 location.longitude.degrees = (frame[9]);
                 location.longitude.minutes = (frame[10] & 0xF8) >> 3;
@@ -144,10 +145,72 @@ void Beacon::parseFrame()
                         location.longitude.minutes -= 1;
                     }
                 }
+                // Identification data in bits 41-58
             }
-            else if ((protocolCode > 1 && protocolCode < 8 )|| protocolCode == 12 /* Ship Security Protocol */ || protocolCode == 14 /* Standard Test Location Prtocol */)
-            {   //Std loc protocol trame longue
-                latoffset = (frame[14] & 0x80) >> 7;  //Latitude STD LOCATION
+            else if ((protocolCode > 1 && protocolCode < 8 )|| protocolCode == 0b1100 /* Ship Security Protocol */ || protocolCode == 0b1110 /* Standard Test Location Prtocol */)
+            {   //Std loc protocol
+                latoffset = (frame[14] & 0x20) >> 5;
+                location.latitude.orientation = (frame[8] & 0x20) >> 5; 
+                location.latitude.degrees = ((frame[8] & 0x1F) << 2) | ((frame[9] & 0xC0) >> 6);
+                location.latitude.minutes = (frame[9] & 0x20) >> 5;
+                location.latitude.minutes = (location.latitude.minutes * 30);
+                latofmin = (frame[14] & 0x1E) >> 1;
+                latofsec = ((frame[14] & 0x01) << 3 | (frame[15] & 0xE0) >> 5);
+                latofsec = (latofsec * 4);
+                if (latoffset == 1) 
+                {
+                    location.latitude.minutes += latofmin;
+                    location.latitude.seconds += latofsec;
+                }
+                else if (latoffset == 0) 
+                {
+                    location.latitude.minutes -= latofmin;
+                    if (location.latitude.minutes < 0) 
+                    {
+                        location.latitude.minutes += 60;
+                        location.latitude.degrees -= 1;
+                    }
+                    location.latitude.seconds -= latofsec;
+                    if (location.latitude.seconds < 0) 
+                    {
+                        location.latitude.seconds += 60;
+                        location.latitude.minutes -= 1;
+                    }
+                }
+                
+                lonoffset = (frame[15] & 0x10) >> 4;  //Longitude STD LOCATION
+                location.longitude.orientation = (frame[9] & 0x10) >> 4; 
+                location.longitude.degrees = ((frame[9] & 0x0F) << 4 | (frame[10] & 0xF0) >> 4);
+                location.longitude.minutes = (frame[10] & 0x08) >> 3;
+                location.longitude.minutes = (location.longitude.minutes * 30);
+                lonofmin = (frame[15] & 0x0F);
+                lonofsec = (frame[16] & 0xF0) >> 4;
+                lonofsec = (lonofsec * 4);
+                if (lonoffset == 1) 
+                {
+                    location.longitude.minutes += lonofmin;
+                    location.longitude.seconds += lonofsec;
+                }
+                else if (lonoffset == 0) 
+                {
+                    location.longitude.minutes -= lonofmin;
+                    if (location.longitude.minutes < 0) 
+                    {
+                        location.longitude.minutes += 60;
+                        location.longitude.degrees -= 1;
+                    }
+                    location.longitude.seconds -= lonofsec;
+                    if (location.longitude.seconds < 0) 
+                    {
+                        location.longitude.seconds += 60;
+                        location.longitude.minutes -= 1;
+                    }
+                }
+                // Identification data in bits 41-64
+            }
+            else if((protocolCode == 0b1101) || (protocolCode == 0b1001))
+            {   // RLS Location protocol => protocolCode == 0b1101 / ELT(DT) Location protocol => protocolCode == 0b1001
+                latoffset = (frame[14] & 0x80) >> 7;
                 location.latitude.orientation = (frame[8] & 0x80) >> 7; 
                 location.latitude.degrees = (frame[8] & 0x7F);
                 location.latitude.minutes = (frame[9] & 0xC0) >> 6;
@@ -204,12 +267,11 @@ void Beacon::parseFrame()
                         location.longitude.minutes -= 1;
                     }
                 }
+                // Identification data in bits 41-66
+
             }
             else
-            {   // TODO :  code = 0 and 1 
-                // TODO : ELT(DT) Location protocol => protocolCode == 9
-                // TODO ? : RTS Location protocol => protocolCode == 13
-                
+            {   // Protocole code 0000 and 0001 = Spare : no location
             }
             switch(protocolCode)
             {
@@ -218,47 +280,47 @@ void Beacon::parseFrame()
                     desciption = F("Spare Location Protocol");
                     break;
                 case 0b0010 :
-                    desciption += F("EPIRB - MMSI/Location Protocol");
+                    desciption = F("EPIRB - MMSI/Location Protocol");
                     break;
                 case 0b0011 :
-                    desciption += F("ELT-24-bit Address/Loc. Prot.");
+                    desciption = F("ELT-24-bit Address/Loc. Prot.");
                     break;
                 case 0b0100 :
                 case 0b0101 :
-                    desciption += F("Serial Location Prot. - ELT");
+                    desciption = F("Serial Location Prot. - ELT");
                     break;
                 case 0b0110 :
-                    desciption += F("Serial Location Prot. - EPIRB");
+                    desciption = F("Serial Location Prot. - EPIRB");
                     break;
                 case 0b0111 :
-                    desciption += F("Serial Location Prot. - PLB");
+                    desciption = F("Serial Location Prot. - PLB");
                     break;
                 case 0b1000 :
-                    desciption += F("National Location Prot. - ELT");
+                    desciption = F("National Location Prot. - ELT");
                     break;
                 case 0b1001 :
-                    desciption += F("ELT(DT) Location Protocol");
+                    desciption = F("ELT(DT) Location Protocol");
                     break;
                 case 0b1010 :
-                    desciption += F("National Location Prot.-EPIRB");
+                    desciption = F("National Location Prot.-EPIRB");
                     break;
                 case 0b1011 :
-                    desciption += F("National Location Prot. - PLB");
+                    desciption = F("National Location Prot. - PLB");
                     break;
                 case 0b1100 :
-                    desciption += F("Ship Security");
+                    desciption = F("Ship Security");
                     break;
                 case 0b1101 :
-                    desciption += F("RLS Location Protocol");
+                    desciption = F("RLS Location Protocol");
                     break;
                 case 0b1110 :
-                    desciption += F("Standard Test Location Prot.");
+                    desciption = F("Standard Test Location Prot.");
                     break;
                 case 0b1111 :
-                    desciption += F("National Test Location Prot.");
+                    desciption = F("National Test Location Prot.");
                     break;
                 default:
-                    desciption += F("Unknown");
+                    desciption = F("Unknown");
             }
         }
     }
@@ -294,5 +356,7 @@ void Beacon::parseFrame()
             default:
                 desciption += F("Unknown");
         }
+        // Identification data in bits 40-83
+        // 0x11223344556677
     }
 }
