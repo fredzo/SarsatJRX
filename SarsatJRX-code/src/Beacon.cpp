947339334906cbc28d9e3a76c67c35cc47354c52
Fred BORRY <fborry@free.fr>
2023-08-03 23:45:42 +0200

    Fixed location decoding

--- SarsatJRX-code/src/Beacon.cpp
+++ SarsatJRX-code/src/Beacon.cpp
@@ -16,11 +16,11 @@ void Beacon::parseFrame()
     protocolFlag = (frame[3] & 0x40) >> 6;   // protocol flag     bit 26
     if(protocolFlag)
     {   // User protocol
-        protocolCode = (frame[4] & 0x0F);    // protocol code     bit 37-40
+        protocolCode = ((frame[4] & 0x0E)) >> 1;  // protocol code     bit 37-39
     }
     else
     {   // Other protocols
-        protocolCode = ((frame[4] & 0x0E)) >> 1;  // protocol code     bit 37-39
+        protocolCode = (frame[4] & 0x0F);    // protocol code     bit 37-40
     }
     unsigned long countryCode;
     countryCode = ((frame[3] & 0x3F) << 4 | (frame[4] & 0xF0) >> 4); // country code bit 27-36
@@ -79,7 +79,9 @@ void Beacon::parseFrame()
                     desciption = F("Test User Location Protocol");
                     break;
                 default:
-                    desciption = F("Unknown");
+                    desciption = "Unknown Protocol (";
+                    desciption += protocolCode;
+                    desciption += ")";
             }
             // Identification data in bits 40-83
         }
@@ -149,67 +151,6 @@ void Beacon::parseFrame()
             }
             else if ((protocolCode > 1 && protocolCode < 8 )|| protocolCode == 0b1100 /* Ship Security Protocol */ || protocolCode == 0b1110 /* Standard Test Location Prtocol */)
             {   //Std loc protocol
-                latoffset = (frame[14] & 0x20) >> 5;
-                location.latitude.orientation = (frame[8] & 0x20) >> 5; 
-                location.latitude.degrees = ((frame[8] & 0x1F) << 2) | ((frame[9] & 0xC0) >> 6);
-                location.latitude.minutes = (frame[9] & 0x20) >> 5;
-                location.latitude.minutes = (location.latitude.minutes * 30);
-                latofmin = (frame[14] & 0x1E) >> 1;
-                latofsec = ((frame[14] & 0x01) << 3 | (frame[15] & 0xE0) >> 5);
-                latofsec = (latofsec * 4);
-                if (latoffset == 1) 
-                {
-                    location.latitude.minutes += latofmin;
-                    location.latitude.seconds += latofsec;
-                }
-                else if (latoffset == 0) 
-                {
-                    location.latitude.minutes -= latofmin;
-                    if (location.latitude.minutes < 0) 
-                    {
-                        location.latitude.minutes += 60;
-                        location.latitude.degrees -= 1;
-                    }
-                    location.latitude.seconds -= latofsec;
-                    if (location.latitude.seconds < 0) 
-                    {
-                        location.latitude.seconds += 60;
-                        location.latitude.minutes -= 1;
-                    }
-                }
-                
-                lonoffset = (frame[15] & 0x10) >> 4;  //Longitude STD LOCATION
-                location.longitude.orientation = (frame[9] & 0x10) >> 4; 
-                location.longitude.degrees = ((frame[9] & 0x0F) << 4 | (frame[10] & 0xF0) >> 4);
-                location.longitude.minutes = (frame[10] & 0x08) >> 3;
-                location.longitude.minutes = (location.longitude.minutes * 30);
-                lonofmin = (frame[15] & 0x0F);
-                lonofsec = (frame[16] & 0xF0) >> 4;
-                lonofsec = (lonofsec * 4);
-                if (lonoffset == 1) 
-                {
-                    location.longitude.minutes += lonofmin;
-                    location.longitude.seconds += lonofsec;
-                }
-                else if (lonoffset == 0) 
-                {
-                    location.longitude.minutes -= lonofmin;
-                    if (location.longitude.minutes < 0) 
-                    {
-                        location.longitude.minutes += 60;
-                        location.longitude.degrees -= 1;
-                    }
-                    location.longitude.seconds -= lonofsec;
-                    if (location.longitude.seconds < 0) 
-                    {
-                        location.longitude.seconds += 60;
-                        location.longitude.minutes -= 1;
-                    }
-                }
-                // Identification data in bits 41-64
-            }
-            else if((protocolCode == 0b1101) || (protocolCode == 0b1001))
-            {   // RLS Location protocol => protocolCode == 0b1101 / ELT(DT) Location protocol => protocolCode == 0b1001
                 latoffset = (frame[14] & 0x80) >> 7;
                 location.latitude.orientation = (frame[8] & 0x80) >> 7; 
                 location.latitude.degrees = (frame[8] & 0x7F);
@@ -245,6 +186,67 @@ void Beacon::parseFrame()
                 location.longitude.minutes = (frame[10] & 0x18) >> 3;
                 location.longitude.minutes = (location.longitude.minutes * 15);
                 lonofmin = (frame[15] & 0x1F);
+                lonofsec = (frame[16] & 0xF0) >> 4;
+                lonofsec = (lonofsec * 4);
+                if (lonoffset == 1) 
+                {
+                    location.longitude.minutes += lonofmin;
+                    location.longitude.seconds += lonofsec;
+                }
+                else if (lonoffset == 0) 
+                {
+                    location.longitude.minutes -= lonofmin;
+                    if (location.longitude.minutes < 0) 
+                    {
+                        location.longitude.minutes += 60;
+                        location.longitude.degrees -= 1;
+                    }
+                    location.longitude.seconds -= lonofsec;
+                    if (location.longitude.seconds < 0) 
+                    {
+                        location.longitude.seconds += 60;
+                        location.longitude.minutes -= 1;
+                    }
+                }
+                // Identification data in bits 41-64
+            }
+            else if((protocolCode == 0b1101) || (protocolCode == 0b1001))
+            {   // RLS Location protocol => protocolCode == 0b1101 / ELT(DT) Location protocol => protocolCode == 0b1001
+                latoffset = (frame[14] & 0x20) >> 5;
+                location.latitude.orientation = (frame[8] & 0x20) >> 5; 
+                location.latitude.degrees = ((frame[8] & 0x1F) << 2) | ((frame[9] & 0xC0) >> 6);
+                location.latitude.minutes = (frame[9] & 0x20) >> 5;
+                location.latitude.minutes = (location.latitude.minutes * 30);
+                latofmin = (frame[14] & 0x1E) >> 1;
+                latofsec = ((frame[14] & 0x01) << 3 | (frame[15] & 0xE0) >> 5);
+                latofsec = (latofsec * 4);
+                if (latoffset == 1) 
+                {
+                    location.latitude.minutes += latofmin;
+                    location.latitude.seconds += latofsec;
+                }
+                else if (latoffset == 0) 
+                {
+                    location.latitude.minutes -= latofmin;
+                    if (location.latitude.minutes < 0) 
+                    {
+                        location.latitude.minutes += 60;
+                        location.latitude.degrees -= 1;
+                    }
+                    location.latitude.seconds -= latofsec;
+                    if (location.latitude.seconds < 0) 
+                    {
+                        location.latitude.seconds += 60;
+                        location.latitude.minutes -= 1;
+                    }
+                }
+                
+                lonoffset = (frame[15] & 0x10) >> 4;
+                location.longitude.orientation = (frame[9] & 0x10) >> 4; 
+                location.longitude.degrees = ((frame[9] & 0x0F) << 4 | (frame[10] & 0xF0) >> 4);
+                location.longitude.minutes = (frame[10] & 0x08) >> 3;
+                location.longitude.minutes = (location.longitude.minutes * 30);
+                lonofmin = (frame[15] & 0x0F);
                 lonofsec = (frame[16] & 0xF0) >> 4;
                 lonofsec = (lonofsec * 4);
                 if (lonoffset == 1) 
@@ -320,7 +322,9 @@ void Beacon::parseFrame()
                     desciption = F("National Test Location Prot.");
                     break;
                 default:
-                    desciption = F("Unknown");
+                    desciption = "Unknown Protocol (";
+                    desciption += protocolCode;
+                    desciption += ")";
             }
         }
     }
@@ -354,7 +358,9 @@ void Beacon::parseFrame()
                 desciption += F("Test");
                 break;
             default:
-                desciption += F("Unknown");
+                desciption += "Unknown (";
+                desciption += protocolCode;
+                desciption += ")";
         }
         // Identification data in bits 40-83
         // 0x11223344556677
