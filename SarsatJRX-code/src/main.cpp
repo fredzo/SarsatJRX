7805f324e4aba54fd38cf2058957c891224a4e41
Fred BORRY <fborry@free.fr>
2023-08-11 18:20:32 +0200

    Fixed frame start detection

--- SarsatJRX-code/src/main.cpp
+++ SarsatJRX-code/src/main.cpp
@@ -33,7 +33,7 @@
 // #define DEBUG_RAM
 
 // Enable decode debuging
-//#define DEBUG_DECODE
+#define DEBUG_DECODE
 
 // Header
 #define HEADER_HEIGHT     20
@@ -85,6 +85,7 @@ const int notificationPin = 19;
 unsigned long microseconds;
 unsigned long modDuration;
 unsigned long frameStartTime;
+int frameStartCount = 0;
 bool frameStarted;
 bool lastModState = 1; // Start with 1
 bool currentBitValue = 1; // Start with 1
@@ -190,7 +191,8 @@ void resetFrameReading()
   frameParseState = 0;
   currentByte = 0;
   lastModState = 1;
-  currentBitValue = 1; // (?)
+  currentBitValue = 1; // Frame always start with 1 bits
+  frameStartCount = 0;
   frameStarted = false;
   // Clear frame content
   for ( byte i = 0; i < Beacon::SIZE; i++)
@@ -207,13 +209,17 @@ void resetFrameReading()
 #endif
 }
 
-#define BIT_DURATION        2500UL            // Bit duration = 2.5 ms = 2500 us
-#define MOD_DURATION        BIT_DURATION/2    // Modulation duration 1.25 ms = 1250 us (half a bit)
-#define TOLERANCE           625UL             // Tolerate a 625 us time deviation
-#define SAME_PHASE_START    MOD_DURATION-TOLERANCE // 1250 - 625 = 625 us
-#define SAME_PHASE_END      MOD_DURATION+TOLERANCE // 1250 + 625 = 1875 us
-#define CHANGE_PHASE_START  (2*MOD_DURATION)-TOLERANCE // 2500 - 625 = 1875 us
-#define CHANGE_PHASE_END    (2*MOD_DURATION)+TOLERANCE // 2500 + 625 = 3125 us
+#define BIT_DURATION        2500UL                        // Bit duration = 2.5 ms = 2500 us
+#define MOD_DURATION        BIT_DURATION/2                // Modulation duration 1.25 ms = 1250 us (half a bit)
+#define TOLERANCE           625UL                         // Tolerate a 625 us time deviation
+#define START_TOLERANCE     250UL                         // Tolerate a 250 us time deviation for frame start detection
+#define FRAME_START_SIZE    29                            // Frame synchronisation pattern is 15 bits i.e. 30 modulations
+#define START_PHASE_START   MOD_DURATION-START_TOLERANCE  // 1250 - 250 = 1000 us
+#define START_PHASE_END     MOD_DURATION+START_TOLERANCE  // 1250 + 250 = 1500 us
+#define SAME_PHASE_START    MOD_DURATION-TOLERANCE        // 1250 - 625 = 625 us
+#define SAME_PHASE_END      MOD_DURATION+TOLERANCE        // 1250 + 625 = 1875 us
+#define CHANGE_PHASE_START  (2*MOD_DURATION)-TOLERANCE    // 2500 - 625 = 1875 us
+#define CHANGE_PHASE_END    (2*MOD_DURATION)+TOLERANCE    // 2500 + 625 = 3125 us
 
 /***********************************************
   Extraction des bits
@@ -222,62 +228,78 @@ void analyze(void)
 {
   modDuration = micros() - microseconds;
 #ifdef DEBUG_DECODE
-  events[eventCount]=modDuration;
-  eventCount++;
-  if(eventCount>=512)
+  if(eventCount<512)
   {
-    eventCount = 0;
+    events[eventCount]=modDuration;
+    eventCount++;
   }
 #endif
 
-  if(modDuration <= SAME_PHASE_START)
-  { // Debounce too short changes
-    return;
-  }
-  microseconds = micros();
   if(!frameStarted)
-  {
-    frameStartTime = millis();
-    frameStarted = true;
-  }
-  if(modDuration >= SAME_PHASE_START && modDuration < SAME_PHASE_END)
-  { // Same phase
-    if(lastModState)
-    { // New modulation
-      lastModState = 0;
-      // Store new bit and lauch parsing
-      currentByte = currentByte << 1 | currentBitValue;
+  { // Monitor closely first 1250-ish us modulations
+    microseconds = micros();
+    if(modDuration >= START_PHASE_START && modDuration < START_PHASE_END)
+    { // This looks like a frame start
+      frameStartCount ++;
+      currentByte = currentByte << 1 | 1;
       Test();
+      if(frameStartCount >= FRAME_START_SIZE)
+      {
+        frameStarted = true;
+        frameStartTime = millis();
+      }
     }
-    lastModState = !lastModState;
-  }
-  else if(modDuration >= CHANGE_PHASE_START && modDuration < CHANGE_PHASE_END)
-  { // Phase change => bit value change
-    currentBitValue = !currentBitValue;
-    // Store new bit and lauch parsing
-    currentByte = currentByte << 1 | currentBitValue;
-    lastModState = 0;
-    Test();
-  }
-  else 
-  { // We have missed changes
-    int missedModulation = modDuration / MOD_DURATION;
-    // Check if it's an even number of modulations
-    int missedBits = missedModulation/2;
-    // Let's assume we received n times the same value 
-    for(int i = 0 ; i < missedBits ; i++)
+    else
     {
-      currentByte = currentByte << 1 | currentBitValue;
-      Test();
+      frameStartCount = 0;
     }
-    bool odd = ((missedModulation%2) != 0);
-    if(odd) 
-    { // We have a phase change
+  }
+  else
+  { // We are in a frame
+    if(modDuration <= SAME_PHASE_START)
+    { // Debounce too short changes
+      return;
+    }
+    microseconds = micros();
+    if(modDuration >= SAME_PHASE_START && modDuration < SAME_PHASE_END)
+    { // Same phase
+      if(lastModState)
+      { // New modulation
+        lastModState = 0;
+        // Store new bit and lauch parsing
+        currentByte = currentByte << 1 | currentBitValue;
+        Test();
+      }
+      lastModState = !lastModState;
+    }
+    else if(modDuration >= CHANGE_PHASE_START && modDuration < CHANGE_PHASE_END)
+    { // Phase change => bit value change
       currentBitValue = !currentBitValue;
       // Store new bit and lauch parsing
       currentByte = currentByte << 1 | currentBitValue;
       lastModState = 0;
       Test();
+    }
+    else 
+    { // We have missed changes
+      int missedModulation = modDuration / MOD_DURATION;
+      // Check if it's an even number of modulations
+      int missedBits = missedModulation/2;
+      // Let's assume we received n times the same value 
+      for(int i = 0 ; i < missedBits ; i++)
+      {
+        currentByte = currentByte << 1 | currentBitValue;
+        Test();
+      }
+      bool odd = ((missedModulation%2) != 0);
+      if(odd) 
+      { // We have a phase change
+        currentBitValue = !currentBitValue;
+        // Store new bit and lauch parsing
+        currentByte = currentByte << 1 | currentBitValue;
+        lastModState = 0;
+        Test();
+      }
     }
   }
 }
