de7d754b31f9271735cbcd3e5209f61fe3926555
Fred BORRY <fborry@free.fr>
2023-10-04 23:40:03 +0200

    Switched to async mode for dra module

--- SarsatJRX-code/src/Radio.cpp
+++ SarsatJRX-code/src/Radio.cpp
@@ -4,6 +4,11 @@
 // Static members
 Radio *Radio::radioInstance = nullptr;
 int Radio::power = 0;
+String Radio::version;
+bool Radio::on = false;
+DRA818::Parameters Radio::parameters;
+float Radio::scanFrequency = 0;
+bool Radio::scanFreqBusy = false;
 
 void Radio::radioInit()
 {   // Init UART1
@@ -20,27 +25,33 @@ void Radio::radioInit()
 #ifdef DRA818_DEBUG
     dra->set_log(&Serial);
 #endif    
+    dra->handshake_async_cb(Radio::handshakeCallback);
+    dra->group_async_cb(Radio::groupCallback);
+    dra->scan_async_cb(Radio::scanCallback);
+    dra->volume_async_cb(Radio::volumeCallback);
+    dra->filters_async_cb(Radio::filtersCallback);
+    dra->tail_async_cb(Radio::tailCallback);
     dra->rssi_async_cb(Radio::rssiCallback);
-    dra->handshake();
-    dra->group(DRA818_12K5, 406.0, 460.0, 0, 4, 0);
-    dra->volume(8);
-    dra->filters(true, false, true);
-    dra->tail(true);
-    dra->scan(440.125);
-    dra->scan(406.00);
+    dra->version_async_cb(Radio::versionCallback);
+    dra->read_group_async_cb(Radio::readGroupCallback);
+    dra->handshake_async();
+    dra->version_async();
+    dra->group_async(DRA818_12K5, 406.0, 460.0, 0, 4, 0);
+    dra->volume_async(8);
+    dra->filters_async(true, false, true);
+    dra->tail_async(true);
+    dra->scan_async(440.125);
+    /*dra->scan(406.00);
     dra->scan(406.025);
     dra->scan(406.028);
     dra->scan(406.037);
     dra->scan(406.040);
     dra->scan(406.049);
     dra->scan(433.000);
-    dra->scan(433.125);
-    Serial.printf("Rssi = %d\n",dra->rssi());
-    Serial.printf("Version = %s\n",dra->version().c_str());
+    dra->scan(433.125);*/
     dra->read_group_async();
     dra->group_async(DRA818_25K, 406.025, 433.000, 25, 2, 38);
     dra->read_group_async();
-    on = true;
 }
 
 void Radio::rssiCallback(int rssi)
@@ -50,7 +61,62 @@ void Radio::rssiCallback(int rssi)
 
 void Radio::readGroupCallback(DRA818::Parameters parameters)
 {
-    Serial.println(parameters.toString().c_str());
+    Radio::parameters = parameters;
+    #ifdef SERIAL_OUT
+        Serial.println(parameters.toString().c_str());
+    #endif
+}
+
+void Radio::groupCallback(int retCode)
+{
+    #ifdef SERIAL_OUT
+        retCode ? Serial.println("Group command success.") : Serial.println("Group command failed.");
+    #endif
+}
+
+void Radio::handshakeCallback(int retCode)
+{
+    on = retCode;
+    #ifdef SERIAL_OUT
+        retCode ? Serial.println("Handshake command success.") : Serial.println("Handshake command failed.");
+    #endif
+}
+
+void Radio::scanCallback(int retCode)
+{
+    scanFreqBusy = retCode;
+    #ifdef SERIAL_OUT
+        Serial.printf("Frequency %3.4f scan : %s\n",scanFrequency, scanFreqBusy ? "busy" : "no signal");
+    #endif
+}
+
+void Radio::volumeCallback(int retCode)
+{
+    #ifdef SERIAL_OUT
+        retCode ? Serial.println("Volume command success.") : Serial.println("Volume command failed.");
+    #endif
+}
+
+void Radio::filtersCallback(int retCode)
+{
+    #ifdef SERIAL_OUT
+        retCode ? Serial.println("Filters command success.") : Serial.println("Filters command failed.");
+    #endif
+}
+
+void Radio::versionCallback(String version)
+{
+    Radio::version = version;
+    #ifdef SERIAL_OUT
+        Serial.printf("Radio module version : %s\n",version);
+    #endif
+}
+
+void Radio::tailCallback(int retCode)
+{
+    #ifdef SERIAL_OUT
+        retCode ? Serial.println("Tail command success.") : Serial.println("Tail command failed.");
+    #endif
 }
 
 
@@ -63,10 +129,7 @@ void Radio::radioStop()
 
 void Radio::handleTask()
 {
-    if(on)
-    {
-        dra->async_task();
-    }
+    dra->async_task();
 }
 
 int Radio::getPower()
--- SarsatJRX-code/src/Radio.h
+++ SarsatJRX-code/src/Radio.h
@@ -35,14 +35,25 @@ class Radio
 
         static void rssiCallback(int rssi);
         static void readGroupCallback(DRA818::Parameters parameters);
+        static void groupCallback(int retCode);
+        static void handshakeCallback(int retCode);
+        static void scanCallback(int retCode);
+        static void volumeCallback(int retCode);
+        static void filtersCallback(int retCode);
+        static void versionCallback(String version);
+        static void tailCallback(int retCode);
 
         HardwareSerial *radioSerial;
         DRA818 *dra;
         float radioFrequency; 
-        bool on = false;
 
         // Static members
         static int power;
+        static bool on;
+        static DRA818::Parameters parameters;
+        static float scanFrequency;
+        static bool scanFreqBusy;
+        static String version;
 
         static Radio *radioInstance;
 };
