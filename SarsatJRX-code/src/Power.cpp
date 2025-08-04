31a38559ff0eb2e9d78cb1bd794777519d184087
Frederic Borry <fborry@lyra-network.com>
2025-08-04 10:56:26 +0200

    Fixed power charge detection timing.

--- SarsatJRX-code/src/Power.cpp
+++ SarsatJRX-code/src/Power.cpp
@@ -5,8 +5,8 @@
 #define MAX_BATTERY_VOLTAGE     4.19
 #define POWER_SAMPLE_PERIOD     200  // 200ms
 #define POWER_UPDATE_PERIOD     1000 // 1s
-#define CHARGE_SAMPLE_PERIOD    50   // 50ms
-#define CHARGE_SAMPLE_COUNT     15   // Wait for 15 consecutive samples with same value to validate charge state
+#define CHARGE_SAMPLE_PERIOD    30   // 30ms
+#define CHARGE_SAMPLE_COUNT     20   // Wait for 15 consecutive samples with same value to validate charge state
 
 // Static members
 Power *Power::powerInstance = nullptr;
