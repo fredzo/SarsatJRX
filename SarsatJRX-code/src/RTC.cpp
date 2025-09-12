db1fa55dfcbfc465fe12a0073a630729a0f86078
fredzo <fborry@free.fr>
2025-09-12 21:13:37 +0200

    Fixed NTP time sync.

--- SarsatJRX-code/src/RTC.cpp
+++ SarsatJRX-code/src/RTC.cpp
@@ -87,11 +87,11 @@ Rtc::Date Rtc::getDate()
             int originalYear = dt.year;
             dt.year = 2015;
             setSystemTime(&dt,false);
-            if(getLocalTime(&timeinfo,0))
+            if(getLocalTime(&timeinfo,500))
             {   // Update RTC
                 dt.day = timeinfo.tm_mday;
                 dt.month = timeinfo.tm_mon+1;
-                dt.year = timeinfo.tm_year;
+                dt.year = timeinfo.tm_year+1900;
                 dt.hour = timeinfo.tm_hour;
                 dt.minute = timeinfo.tm_min;
                 dt.second = timeinfo.tm_sec;
@@ -109,6 +109,7 @@ Rtc::Date Rtc::getDate()
             }
             else 
             {   // Restore original date
+                Serial.println("Could not get time from NTP server.");
                 dt.year = originalYear;
                 setSystemTime(&dt,false);
             }
